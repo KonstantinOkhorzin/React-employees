a0ddfa5abf941de951d3f7d844b26db630d2c21d
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2021-11-12 14:25:49 +0200

    add props key

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,9 +4,10 @@ import './employees-list.css';
 const EmployeesList = ({data}) => {
 
     const elements = data.map(item => {
+        const {id, ...itemProps} = item;// вытаскиваем id (деструкторизация по остаточному принцыпу)
         return (
             // <EmployeesListItem name={item.name} salary={item.salary}/>
-            <EmployeesListItem {...item}/> //  или через Spread оператор
+            <EmployeesListItem key={id} {...itemProps}/> //  или через Spread оператор
         )
     })
     return(
