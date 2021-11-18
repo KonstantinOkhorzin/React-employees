40e455e9cee1344e4f38a20375d1e63d2d87c54e
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2021-11-18 15:43:54 +0200

    add delete item list

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,13 +1,14 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;// вытаскиваем id (деструкторизация по остаточному принцыпу)остальные props записываем в обьект itemProps 
         return (
-            // <EmployeesListItem name={item.name} salary={item.salary}/>
-            <EmployeesListItem key={id} {...itemProps}/> //  или через Spread оператор
+            <EmployeesListItem key={id} 
+            {...itemProps} //  или через Spread оператор
+            onDelete={() => onDelete(id)}/> 
         )
     })
     return(
