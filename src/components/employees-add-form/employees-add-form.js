95371520d3a06229ea7babc3ee3a89310217d951
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2021-12-27 22:27:48 +0200

    считываем данные с инпутов и записываем в стате

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 import './employees-add-form.css';
 
 const EmployeesAddForm = () => {
+
+    const [name, setName] = useState('');
+    const [salary, setSalary] = useState('');
+
     return (
         <div className="app-add-form">
             <h3>Добавить нового сотрудника</h3>
             <form className="add-form d-flex">
-                <input type="text" className="form-control new-post-label" placeholder="Как его зовут"/>
-                <input type="number" className="form-control new-post-label" placeholder="З/П в $?"/>
+                <input
+                    onChange={e => setName(e.target.value)}
+                    value={name}
+                    type="text" 
+                    className="form-control new-post-label" 
+                    placeholder="Как его зовут"/>
+                <input 
+                    onChange={e => setSalary(e.target.value)}
+                    value={salary}
+                    type="number" 
+                    className="form-control new-post-label" 
+                    placeholder="З/П в $?"/>
                 <button type="submit" className="btn btn-outline-light">Добавить</button>
             </form>
         </div>
